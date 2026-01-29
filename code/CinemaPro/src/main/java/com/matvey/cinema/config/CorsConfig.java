83a7f67635cabd25ae90acc075c518433d99a1f9
dimin7226 <dimin7226@gmail.com>
2026-01-29 09:41:18 +0300

    Add frontend to docker-compose

---
+++ code/CinemaPro/src/main/java/com/matvey/cinema/config/CorsConfig.java
@@ -0,0 +1,20 @@
+// Создайте новый класс конфигурации
+package com.matvey.cinema.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:80", "http://localhost")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
